50d8bfd77fbc04d3fb38c0681ac8b405a72c183e
nirav2000 <nirav2000+github@gmail.com>
2026-09-17 15:04:25 +0100

    Export OpenDay sync endpoint

--- functions/index.js
+++ functions/index.js
@@ -25,3 +25,4 @@ export const explain=onCall({region:'europe-west2',secrets:[key],maxInstances:1,
 });
 
 export {createBeyond100ReviewLink,revokeBeyond100ReviewLink,beyond100Review} from './beyond100-review.js';
+export {opendaySync} from './openday-sync.js';
